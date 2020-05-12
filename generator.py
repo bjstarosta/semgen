0488e33f14dd53527f1090befdf11d109f1ac2f0
Bohdan Starosta <bjstarosta@gmail.com>
2020-05-12 03:50:27 +0100

    Fixing docs

--- generator.py
+++ generator.py
@@ -395,39 +395,11 @@ class GoldOnCarbonGenerator(Generator):
 
 
 class GaNSurfaceGenerator(Generator):
-    """The main class controlling the generator and filesystem operations.
-
-    If the class has public attributes, they may be documented here
-    in an ``Attributes`` section and follow the same formatting as a
-    function's ``Args`` section. Alternatively, attributes may be documented
-    inline with the attribute's declaration (see __init__ method below).
-
-    Properties created with the ``@property`` decorator should be documented
-    in the property's getter method.
+    """Simulates GaN surface images of dislocations.
 
     Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
 
     """
 
     def __init__(self):
-        """Example of docstring on the __init__ method.
-
-        The __init__ method may be documented in either the class level
-        docstring, or as a docstring on the __init__ method itself.
-
-        Either form is acceptable, but the two should not be mixed. Choose one
-        convention to document the __init__ method and be consistent with it.
-
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-
-        Args:
-            param1 (str): Description of `param1`.
-            param2 (:obj:`int`, optional): Description of `param2`. Multiple
-                lines are supported.
-            param3 (:obj:`list` of :obj:`str`): Description of `param3`.
-
-        """
         pass
